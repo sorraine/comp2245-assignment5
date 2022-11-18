30df5dfb03721789f52a47a4af0f5e3e46621d12
Kalifa Roberts <kalifasroberts@gmail.com>
2022-11-17 21:56:25 -0400

    Edited

--- world.js
+++ world.js
@@ -1,24 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
+    var btn = document.querySelector("#lookup");
 
-    let btn = document.querySelector("#lookup");
-
-    btn.addEventListener("click", function(event){
+    btn.addEventListener("click", function(event) {
         event.preventDefault();
 
-
-        let value = document.querySelector("#country").value;
+        var value = document.querySelector("#country").value;
         console.log(value);
 
-
-        fetch('http://localhost/comp2245-assignment5/world.php?country=${value}')
+        fetch(`http://localhost/comp2245-assignment5/world.php?country=${value}`)
         .then(response => response.text())
-        .then (data => {
-            let result = document.querySelector("result");
-            result.innerHTML = data;
+        .then(data => {
+             var result = document.querySelector("#result");
+             result.innerHTML = data;
         })
         .catch(error => {
 
         })
     })
-
 })
--- world.php
+++ world.php
@@ -1,17 +1,32 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
+$password = '';
 $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$stmt = $conn->query("SELECT * FROM countries");
 $country = $_GET['country'];
-  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'")
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo "<ul>";
+
+echo "<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of State</th>
+  </tr>";
 foreach ($results as $row) {
-  echo "<li>" . $row['name'] . ' is ruled by ' . $row['head_of_state'] . "</li>";
+  echo "<tr>";
+  echo "<td>" . $row['name'] . "</td>";
+  echo "<td>" . $row['continent'] . "</td>";
+  echo "<td>" . $row['independence_year'] . "</td>";
+  echo "<td>" . $row['head_of_state'] . "</td>";
+  echo "</tr>";
 }
-echo "</ul>";
+echo "</table>";
+
 ?>
